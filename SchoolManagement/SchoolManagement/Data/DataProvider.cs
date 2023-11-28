c00e17628f840404458f924fb1b306a4f8d45b71
Thông Phạm Duy <21522645@gm.uit.edu.vn>
2023-11-28 21:38:27 +0700

    Finish 2 task: connect database to project and singleton designpattern

---
+++ SchoolManagement/SchoolManagement/Data/DataProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Data
+{
+    public class DataProvider
+    {
+        private static SchoolManagement schoolManagement;
+
+        public static SchoolManagement SchoolManagement
+        {
+            get 
+            { 
+                if (schoolManagement == null)
+                {
+                    schoolManagement = new SchoolManagement();
+                }
+
+                return schoolManagement; 
+            }
+            set { schoolManagement = value; }
+        }
+
+    }
+}
