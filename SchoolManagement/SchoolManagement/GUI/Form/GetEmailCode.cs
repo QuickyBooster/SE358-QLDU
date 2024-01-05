b3278158dff093558000e46927e18df0055c02f6
QuickyBooster <quicklybooster.vh@gmail.com>
2024-01-05 20:58:52 +0700

    a part of recover account

--- SchoolManagement/SchoolManagement/GUI/Form/GetEmailCode.cs
+++ SchoolManagement/SchoolManagement/GUI/Form/GetEmailCode.cs
@@ -10,16 +10,31 @@ using System.Windows.Forms;
 
 namespace SchoolManagement
 {
-    public partial class GetEmailCode : Form
-    {
-        public GetEmailCode()
-        {
-            InitializeComponent();
-        }
+	public partial class GetEmailCode : Form
+	{
+		private int code;
+		public GetEmailCode()
+		{
+			InitializeComponent();
+		}
+		public GetEmailCode(int code)
+		{
+			InitializeComponent();
+			this.code = code;
+		}
 
 		private void btn_confirm_Click(object sender, EventArgs e)
 		{
+			try
+			{
+				if (txtEmailCode.Text.Equals(code.ToString()))
+				{
 
+				}
+			} catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
 		}
 	}
 }
