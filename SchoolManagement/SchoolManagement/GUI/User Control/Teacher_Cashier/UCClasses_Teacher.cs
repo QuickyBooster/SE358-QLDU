bf3aa93853f02e8fa59bd7bbeb6491c0ddc859ce
QuickyBooster <quicklybooster.vh@gmail.com>
2024-01-06 11:08:34 +0700

    add function in class view + find class + display class + view class detail

--- SchoolManagement/SchoolManagement/GUI/User Control/Teacher_Cashier/UCClasses_Teacher.cs
+++ SchoolManagement/SchoolManagement/GUI/User Control/Teacher_Cashier/UCClasses_Teacher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolManagement.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,67 @@ namespace SchoolManagement
 {
     public partial class UCClasses_Teacher : UserControl
     {
-        public UCClasses_Teacher()
-        {
-            InitializeComponent();
-        }
+		private User user;
+		public UCClasses_Teacher(User user)
+		{
+			InitializeComponent();
+			this.user = user;
+			LoadData(DataProvider.SchoolManagement.Classes.AsNoTracking().ToList());
+		}
 
-        private void viewAllButton_Click(object sender, EventArgs e)
+		private void LoadData(List<Class> classes)
+		{
+			lbTotaClasses.Text = classes.ToList().Count.ToString();
+
+			gridviewSupplier.Rows.Clear();
+			foreach (Class c in classes)
+			{
+				int total = 0;
+				foreach (var p in DataProvider.SchoolManagement.Students)
+				{
+					if (p.Class.ClassID == c.ClassID)
+					{
+						total++;
+					}
+				}
+				
+				gridviewSupplier.Rows.Add(c.ClassID, c.Grade, c.ClassName, total, c.User.FulName);
+			}
+		}
+
+		private void btnAddClass_Click(object sender, EventArgs e)
+		{
+			AddNewClass addNewClass = new AddNewClass();
+			addNewClass.ShowDialog();
+			LoadData(DataProvider.SchoolManagement.Classes.AsNoTracking().ToList());
+		}
+
+		private void gridviewSupplier_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+		{
+			int idx = e.RowIndex;
+			if (idx < 0) return;
+			else
+			{
+				var editClass = new AddNewClass(int.Parse(gridviewSupplier.Rows[idx].Cells["ClassId"].Value.ToString()));
+				editClass.ShowDialog();
+				LoadData(DataProvider.SchoolManagement.Classes.AsNoTracking().ToList());
+			}
+		}
+
+		private void txtSearchClass_TextChanged(object sender, EventArgs e)
+		{
+			var list = new List<Class>();
+			foreach (var p in DataProvider.SchoolManagement.Classes.ToList())
+			{
+				if (p.ClassName.ToLower().Contains(txtSearchClass.Text.ToLower()))
+				{
+					list.Add(p);
+				}
+			}
+			LoadData(list);
+		}
+
+		private void viewAllButton_Click(object sender, EventArgs e)
         {
             Form viewAllExams = new View_All_Exams();
             viewAllExams.Show();
