d3a52fa5a334a9c2bf64fe843cbf66ac0cd57935
Thông Phạm Duy <21522645@gm.uit.edu.vn>
2024-01-06 13:18:52 +0700

    Fix bug

--- SchoolManagement/SchoolManagement/GUI/Form/Student CRUD/UCStudentScoreInfo.Designer.cs
+++ SchoolManagement/SchoolManagement/GUI/Form/Student CRUD/UCStudentScoreInfo.Designer.cs
@@ -38,12 +38,13 @@
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.btnSearch = new System.Windows.Forms.PictureBox();
             this.gridviewClass = new Guna.UI2.WinForms.Guna2DataGridView();
-            //this.ExamID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbStudentName = new System.Windows.Forms.Label();
+            this.btnEdit = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbAVGPoint = new System.Windows.Forms.Label();
             this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FifteenMins = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.OnePeriodScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lbStudentName = new System.Windows.Forms.Label();
-            this.btnEdit = new Guna.UI2.WinForms.Guna2GradientButton();
             this.label1 = new System.Windows.Forms.Label();
             this.lbAVGPoint = new System.Windows.Forms.Label();
             //this.lbStudentName = new System.Windows.Forms.Label();
